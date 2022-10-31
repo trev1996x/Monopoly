7ff4fe4d8503a7881512a9e016348ac4fc692b88
trev1996x <115669657+trev1996x@users.noreply.github.com>
2022-10-31 11:01:55 -0500

    Update Game.java
    ..

--- MonopolyGame/src/edu/ilstu/monopoly/Game.java
+++ MonopolyGame/src/edu/ilstu/monopoly/Game.java
@@ -7,66 +7,37 @@ import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.RenderingHints;
-import java.awt.Image;
-import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import java.lang.Thread;
-import java.io.File;
-import java.io.IOException;
-
 import javax.swing.JFrame;
-import javax.imageio.ImageIO;
 
 import edu.ilstu.monopoly.items.*;
 
 class Renderer extends Thread {
 	private boolean toggle_debug = false; // for DebugBox colors
 
-	public void drawImage(Graphics2D g2, String imgSrc, Dimension imgSize) {
-		try {
-			g2.drawImage(
-					ImageIO.read(new File("resources/" + imgSrc)).getScaledInstance(imgSize.width, imgSize.height,
-							Image.SCALE_DEFAULT),
-					0, 0, Color.WHITE,
-					this.gameRef.display.getFocusCycleRootAncestor());
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-	}
-
-	public void drawImage(Graphics2D g2, String imgSrc, int width, int height) {
-		drawImage(g2, imgSrc, new Dimension(width, height));
-	}
-
 	/**
 	 * Show the splash screen
-	 * 
 	 * @param g2 Graphics2D
 	 */
 	public void showSplash(Graphics2D g2) {
-
 		FontMetrics fm;
 		g2.setFont(new Font("Arial", Font.BOLD, 56));
 		g2.setColor(Color.BLACK);
 		fm = g2.getFontMetrics();
-		g2.drawString("ISU Monopoly",
-				(this.frame.getWidth() / 2) - ((int) fm.getStringBounds("ISU Monopoly", g2).getWidth() / 2) + 5,
-				fm.getAscent() + 200 + 5);
+		g2.drawString("ISU Monopoly", (this.frame.getWidth() / 2) - ((int)fm.getStringBounds("ISU Monopoly", g2).getWidth() / 2) + 5, fm.getAscent() + 200 + 5);
 		g2.setColor(Color.RED);
-		g2.drawString("ISU Monopoly",
-				(this.frame.getWidth() / 2) - ((int) fm.getStringBounds("ISU Monopoly", g2).getWidth() / 2),
-				fm.getAscent() + 200);
+		g2.drawString("ISU Monopoly", (this.frame.getWidth() / 2) - ((int)fm.getStringBounds("ISU Monopoly", g2).getWidth() / 2), fm.getAscent() + 200);
 
 		StartButton startButton = new StartButton((this.frame.getWidth() / 2), 275);
 
 		startButton.setLocation(startButton.getX() - (startButton.getBounds().width / 2), startButton.getY());
 
-		if (startButton.isMouseHovering(mousePos)) {
+		if(startButton.isMouseHovering(mousePos)) {
 			startButton.setHover(true);
-			if (mouseClicked)
-				this.gameRef.startPlaying();
-		} else
-			startButton.setHover(false);
+			if(mouseClicked) this.gameRef.startPlaying();
+		}
+		else startButton.setHover(false);
 
 		startButton.render(g2);
 
@@ -74,23 +45,22 @@ class Renderer extends Thread {
 
 		creditsButton.setLocation(creditsButton.getX() - (creditsButton.getBounds().width / 2), creditsButton.getY());
 
-		if (creditsButton.isMouseHovering(mousePos)) {
+		if(creditsButton.isMouseHovering(mousePos)) {
 			creditsButton.setHover(true);
-			if (mouseClicked)
-				creditsButton.showInfoBox((JFrame) this.gameRef.mainWindow);
-		} else
-			creditsButton.setHover(false);
+			if(mouseClicked) creditsButton.showInfoBox((JFrame)this.gameRef.mainWindow);
+		}
+		else creditsButton.setHover(false);
 
 		creditsButton.render(g2);
 
-	}
+		}
 
 	/**
 	 * Render the game
-	 * 
 	 * @param g2 Graphics2D
 	 */
 	public void showGame(Graphics2D g2) {
+		FontMetrics fm;
 
 		DebugBox box = new DebugBox(0, 100);
 
@@ -103,62 +73,62 @@ class Renderer extends Thread {
 		box.render(g2);
 
 		int frameWidth = this.frame.getWidth(),
-				frameHeight = this.frame.getHeight() - 20;
+				frameHeight = this.frame.getHeight();
 
 		/**
 		 * Begins top/north side panel
 		 * x-axis increments by 70
 		 */
 		g2.setColor(Color.BLACK);
-		GameBox box1 = new GameBox(0, 20, frameWidth / 11, frameHeight / 11);
+		GameBox box1 = new GameBox(0, 20, frameWidth / 10, frameHeight / 10);
 		box1.render(g2);
 
 		g2.setColor(Color.BLUE); // set color
-		GameBox box2 = new GameBox(70, 20, frameWidth / 11, frameHeight / 11); // create object
+		GameBox box2 = new GameBox(80, 20, frameWidth / 10, frameHeight / 10); // create object
 		box2.render(g2); // draw object to screen
 
 		g2.setColor(Color.RED);
-		GameBox box3 = new GameBox(140, 20, frameWidth / 11, frameHeight / 11);
+		GameBox box3 = new GameBox(160, 20, frameWidth / 10, frameHeight / 10);
 		box3.render(g2);
 
 		g2.setColor(Color.BLUE);
-		GameBox box4 = new GameBox(210, 20, frameWidth / 11, frameHeight / 11);
+		GameBox box4 = new GameBox(240, 20, frameWidth / 10, frameHeight / 10);
 		box4.render(g2);
 
 		g2.setColor(Color.RED);
-		GameBox box5 = new GameBox(280, 20, frameWidth / 11, frameHeight / 11);
+		GameBox box5 = new GameBox(320, 20, frameWidth / 10, frameHeight / 10);
 		box5.render(g2);
 
 		g2.setColor(Color.BLUE);
-		GameBox box6 = new GameBox(350, 20, frameWidth / 11, frameHeight / 11);
+		GameBox box6 = new GameBox(400, 20, frameWidth / 10, frameHeight / 10);
 		box6.render(g2);
 
 		g2.setColor(Color.RED);
-		GameBox box7 = new GameBox(420, 20, frameWidth / 11, frameHeight / 11);
+		GameBox box7 = new GameBox(480, 20, frameWidth / 10, frameHeight / 10);
 		box7.render(g2);
 
 		g2.setColor(Color.BLUE);
-		GameBox box8 = new GameBox(490, 20, frameWidth / 11, frameHeight / 11);
+		GameBox box8 = new GameBox(560, 20, frameWidth / 10, frameHeight / 10);
 		box8.render(g2);
 
 		g2.setColor(Color.RED);
-		GameBox box9 = new GameBox(560, 20, frameWidth / 11, frameHeight / 11);
+		GameBox box9 = new GameBox(640, 20, frameWidth / 10, frameHeight / 10);
 		box9.render(g2);
 
 		g2.setColor(Color.BLUE);
-		GameBox box10 = new GameBox(630, 20, frameWidth / 11, frameHeight / 11);
+		GameBox box10 = new GameBox(720, 20, frameWidth / 10, frameHeight / 10);
 		box10.render(g2);
-
-		g2.setColor(Color.BLACK);
-		GameBox box11 = new GameBox(700, 20, frameWidth / 11, frameHeight / 11);
-		box11.render(g2);
 
 		/**
 		 * Begins right/east side panel
 		 * y-axis increments by 68
 		 */
+		g2.setColor(Color.BLACK);
+		GameBox box11 = new GameBox(800, 20, frameWidth / 10, frameHeight / 10);
+		box11.render(g2);
 
 		g2.setColor(Color.RED);
+<<<<<<< HEAD
 		GameBox box12 = new GameBox(700, 88, frameWidth / 11, frameHeight / 11);
 		box12.render(g2);
 
@@ -196,6 +166,45 @@ class Renderer extends Thread {
 
 		g2.setColor(Color.BLUE);
 		GameBox box21 = new GameBox(700, 700, frameWidth / 11, frameHeight / 11);
+=======
+		GameBox box12 = new GameBox(800, 20, frameWidth / 10, frameHeight / 10);
+		box12.render(g2);
+
+		g2.setColor(Color.BLUE);
+		GameBox box13 = new GameBox(800, 100, frameWidth / 10, frameHeight / 10);
+		box13.render(g2);
+
+		g2.setColor(Color.RED);
+		GameBox box14 = new GameBox(800, 180, frameWidth / 10, frameHeight / 10);
+		box14.render(g2);
+
+		g2.setColor(Color.BLUE);
+		GameBox box15 = new GameBox(800, 260, frameWidth / 10, frameHeight / 10);
+		box15.render(g2);
+
+		g2.setColor(Color.RED);
+		GameBox box16 = new GameBox(800, 340, frameWidth / 10, frameHeight / 10);
+		box16.render(g2);
+
+		g2.setColor(Color.BLUE);
+		GameBox box17 = new GameBox(800, 420, frameWidth / 10, frameHeight / 10);
+		box17.render(g2);
+
+		g2.setColor(Color.RED);
+		GameBox box18 = new GameBox(800, 500, frameWidth / 10, frameHeight / 10);
+		box18.render(g2);
+
+		g2.setColor(Color.BLUE);
+		GameBox box19 = new GameBox(800, 580, frameWidth / 10, frameHeight / 10);
+		box19.render(g2);
+
+		g2.setColor(Color.RED);
+		GameBox box20 = new GameBox(800, 640, frameWidth / 10, frameHeight / 10);
+		box20.render(g2);
+
+		g2.setColor(Color.BLUE);
+		GameBox box21 = new GameBox(800, 720, frameWidth / 10, frameHeight / 10);
+>>>>>>> parent of b5f3eb5 (Co-authored-by: Ty Qualters <contact@tyqualters.com>)
 		box21.render(g2);
 
 		//
@@ -209,7 +218,6 @@ class Renderer extends Thread {
 
 	/**
 	 * Render the header + FPS counter
-	 * 
 	 * @param g2 Graphics2D
 	 */
 	public void drawHeader(Graphics2D g2) {
@@ -225,16 +233,13 @@ class Renderer extends Thread {
 		this.framesNow += 1L;
 
 		long now = System.currentTimeMillis();
-		if (now - this.lastCounted >= 1_000L) {
+		if(now - this.lastCounted >= 1_000L) {
 			this.lastCounted = now;
 			this.framesThen = this.framesNow;
 			this.framesNow = 0L;
 		}
 
-		g2.drawString(
-				Long.toString(this.framesThen) + " FPS", this.frame.getWidth()
-						- (int) fm.getStringBounds(Long.toString(this.framesThen) + " FPS", g2).getWidth() - 20,
-				fm.getAscent());
+		g2.drawString(Long.toString(this.framesThen) + " FPS", this.frame.getWidth() - (int)fm.getStringBounds(Long.toString(this.framesThen) + " FPS", g2).getWidth() - 20, fm.getAscent());
 	}
 
 	@Override
@@ -244,10 +249,9 @@ class Renderer extends Thread {
 		g2.setComposite(AlphaComposite.Src);
 		g2.setFont(new Font("Arial", Font.BOLD, 18));
 
-		if (!this.gameRef.isPlaying)
-			showSplash(g2);
-		else
-			showGame(g2); // Show the game
+		if(!this.gameRef.isPlaying)
+			showSplash(g2); // Show the splash screen
+		else showGame(g2); // Show the game
 
 		drawHeader(g2);
 
@@ -283,7 +287,6 @@ class Renderer extends Thread {
 
 	/**
 	 * Set the thread's cached Mouse Clicked Status
-	 * 
 	 * @param mouseClicked
 	 */
 	public void setMouseClicked(boolean mouseClicked) {
@@ -331,8 +334,7 @@ public class Game {
 	 */
 	public void run() throws InterruptedException {
 		// Set the front (first) frame to be rendered
-		this.frontFrame = new BufferedImage(this.display.getWidth(), this.display.getHeight(),
-				BufferedImage.TYPE_INT_ARGB);
+		this.frontFrame = new BufferedImage(this.display.getWidth(), this.display.getHeight(), BufferedImage.TYPE_INT_ARGB);
 		this.selectedFrame = this.frontFrame;
 
 		// Initialize the renderer (thread)
